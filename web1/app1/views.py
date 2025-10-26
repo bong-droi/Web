ca39d83c87b46cae0464e1c12369384006f65ae5
bong-droi <lovellzel266@gmail.com>
2025-10-26 12:37:56 +0700

    Delete web1 directory

--- web1/app1/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-#request: yeu cau cuar nguoi dung
-#response: tra ve cho nguoi dung
-
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello day laf app1 nhe!")
